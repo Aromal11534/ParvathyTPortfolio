1cdc80dc9362e70def6524487dea11b0558a9fc7
Aromal <saromalkrishna11534@gmail.com>
2025-10-04 06:18:13 +0530

    third commit

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Smooth scrolling for navigation links
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+    const body = document.querySelector('body');
+
+    // Hamburger menu functionality
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('nav-active');
+        hamburger.classList.toggle('toggle');
+        body.classList.toggle('nav-open');
+    });
+
+    // Smooth scrolling and close menu on link click
     document.querySelectorAll('.nav-links li a').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             // Check if the link is an anchor link
@@ -9,15 +20,14 @@ document.addEventListener('DOMContentLoaded', function() {
                 document.querySelector(this.getAttribute('href')).scrollIntoView({
                     behavior: 'smooth'
                 });
+
+                // Close mobile menu if open
+                if (navLinks.classList.contains('nav-active')) {
+                    navLinks.classList.remove('nav-active');
+                    hamburger.classList.remove('toggle');
+                    body.classList.remove('nav-open');
+                }
             }
         });
-    });
-
-    // Hamburger menu functionality
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
-
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('nav-active');
     });
 });
